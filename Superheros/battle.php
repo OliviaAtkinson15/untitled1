d880ac613cd5eba313c86b2ce84b66bc8c663b3f
OliviaAtkinson15 <1305999@rgu.ac.uk>
2020-03-09 20:49:55 +0000

    Superhero pages successful

---
+++ Superheros/battle.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <title>Superhero System</title>
+</head>
+<body>
+<header>
+    <h1>The Super-Superhero System</h1>
+    <h2>Record a Battle</h2>
+    <p><a href="index.php">Return to home...</a></p>
+</header>
+<main>
+    <form action="insertBattle.php" method="post">
+        <p>Select the superhero that faught in this battle</p><select name="superhero">
+    <?
+    include ("connectUsers.php");
+    $sql_query = "SELECT * FROM superheors";
+    $result = $db->query($sql_query);
+    while($row = $result->fetch_array())
+    {
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
+        $superheroID = $row['superpowerID'];
+        echo "<option value='{$superheroID}'>{$firstname}{$lastname}</option>";
+
+    }
+    ?>
+        </select><br>
+        <input type="text" name="villan" placeholder="Villan Fought"><br>
+        <input type="submit" text="Record Battle">
+    </form>
+</main>
+</body>
+</html>
+
+
+
---
+++ Superheros/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <title>Superhero System</title>
+</head>
+<body>
+<header>
+    <h1>The Super-Superhero System</h1>
+    <h2>Superhero Home Page</h2>
+</header>
+<p>What would you like to do?</p>
+<ul>
+    <li><a href = "insertSuperhero.php">Insert a Superhero</a></li>
+    <li><a href = "displaySuperheros.php">Display all Superheros</a></li>
+    <li><a href = "battle.php">Insert a Battle</a></li>
+    <li><a href = "displayBattles.php">Display All Battles</a></li>
+    <ul>
+        <?
+        include ("connectUsers.php");
+        $sql_query = "SELECT * FROM superheors";
+        $result = $db->query($sql_query);
+        while($row = $result->fetch_array())
+        {
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $id = $row['superheroID'];
+            echo "<li> <a href = 'displayBattles.php?id={$id}'>Battles for {$firstname}{$lastname}</a></li>";
+        }
+        ?>
+    </ul>
+</ul>
+</body>
+</html>
+
+
+
+
