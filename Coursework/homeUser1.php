dff62d51249f8dfb43a0d67f1dbc7cc91760e558
OliviaAtkinson15 <1305999@rgu.ac.uk>
2020-03-18 18:45:24 +0000

    Coursework - 18.03.20

--- Coursework/homeUser1.php
+++ Coursework/homeUser1.php
@@ -6,9 +6,18 @@
 </head>
 <body>
 <header>
-    <h1>IKM Intel</h1>
+
     <img src="ikmlogo.jpeg" alt="IKM Logo">
+    <h1>IKM Intel</h1>
     <a href="Index.php">Back to Login</a>
+
+    <?php
+    include("connectUsers.php");
+    session_start();
+    $email = $_SESSION['email'];
+    echo "<p>". $email . "</p>";
+
+    ?>
 
 </header>
 
@@ -17,24 +26,7 @@
     <li><a href = "quicklinksUser.php">Quick Links</a></li>
     <li><a href = "discountsUser.php">Discounts</a></li>
     <li><a href = "suggestionboxUser.php">Suggestion Box</a></li>
-    <ul>
-
-        <a href="Index.php">Back to Login</a>
-        <?php
-        include("connectUsers.php");
-        session_start();
-        $_SESSION['email'] = $email;
-        $sql_query = "SELECT * FROM members";
-        $result = $db->query($sql_query);
-        while($row = $result->fetch_array())
-        {
-            $email = $row['email'];
-            $password = $row['password'];
-            $id = $row['uid'];
-            echo "<li> <a href = 'displayBattles.php?id={$id}'>Battles for {$firstname}{$lastname}</a></li>";
-        }
-        ?>
-    </ul>
 </ul>
+
 </body>
 </html>
